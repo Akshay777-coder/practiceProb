90fa07ae707192141e8bf8847c5755f82ccc5fea
Akshay Kumar <akshaykumar954012@gmail.com>
2026-04-16 14:38:01 +0530

    added a java dir with Main.java and Main.class

---
+++ java/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = new String("Harry");
+        int a = 8;
+        float b = 4.728282f;
+        System.out.printf("The value of a is %d and the value of b is %f\n",a,b);
+    }
+}
