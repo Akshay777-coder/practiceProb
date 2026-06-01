2d34cbeef78f5af3b35d8ff4b2708e6aedc5b57a
Akshay Kumar <akshaykumar954012@gmail.com>
2026-06-01 16:39:20 +0530

    added trim function

--- java/chw_14_string_methods.java
+++ java/chw_14_string_methods.java
@@ -13,6 +13,8 @@ public class chw_14_string_methods {
         String ustring = name.toUpperCase();
         System.out.println(ustring);
         //strings are immutable that's why we are assigning it to the new string...
-
+        String nonTrimmedString = "    harry    ";
+        System.out.println(nonTrimmedString);
+        System.out.println(nonTrimmedString.trim());
     }
 }
