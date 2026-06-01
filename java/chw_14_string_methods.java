297eea33cb9715a706432e23a9dc4925282e1a73
Akshay Kumar <akshaykumar954012@gmail.com>
2026-06-01 16:19:12 +0530

    added new string methods

---
+++ java/chw_14_string_methods.java
@@ -0,0 +1,7 @@
+public class chw_14_string_methods {
+    public static void main(String[] args) {
+        String a = "Harry";
+        //String a = new String("harry");
+        System.out.println(a);
+    }
+}
